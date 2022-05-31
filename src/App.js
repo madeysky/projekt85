ceb2f61b7992eb6addd056a4ae40f9693cf1c57c
madeysky <pawelrmadej@gmail.com>
2022-05-31 20:27:44 +0200

    ustawieniaPocz

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Gora from "./komponenty/gora/Gora";
+import Napisz from "./strony/napisz/Napisz";
 import Pojedynczy from "./strony/pojedynczy/Pojedynczy";
+import Ustawienia from "./strony/ustawienia/Ustawienia";
 
 function App() {
   return (
     <>
     <Gora/>
-    <Pojedynczy/>
+    <Ustawienia/>
     </>
   )
 }
---
+++ src/strony/ustawienia/Ustawienia.jsx
@@ -0,0 +1,10 @@
+import "./ustawienia.css"
+
+
+export default function Ustawienia() {
+    return (
+        <div className="ustawienia">
+            Ustawienia
+        </div>
+    )
+}
